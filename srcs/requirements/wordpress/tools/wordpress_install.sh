1dc6fbd237912f7685342926766b69ef228a03d2
JeremyVanderMotte <jeremy.vandermotte@gmail.com>
2023-02-02 15:00:10 +0100

    trying wordpress (error www.conf)

---
+++ srcs/requirements/wordpress/tools/wordpress_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -f ./wp-config.php ]
+then
+	echo "wordpress already install"
+else
+
+	wget http://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
+	mv wordpress/* .
+	rm -rf latest.tar.gz
+
+	sed -i "s/database_name_here/$DBAME/g" wp-config-sample.php
+	sed -i "s/username_here/$DBUSER/g" wp-config-sample.php
+	sed -i "s/password_here/$DBPWD/g" wp-config-sample.php
+	sed -i "s/localhost/$DBHOST/g" wp-config-sample.php
+
+	cp wp-config-sample.php wp-config.php
+
+	sed -i "s/listen =/0.0.0.0:9000/g" /etc/php/7.4/fpm/pool.d/www.conf
+fi
