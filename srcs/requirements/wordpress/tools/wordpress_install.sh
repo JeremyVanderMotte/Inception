b777cdcbb0ee24d90032754fd745619a76cba2fb
JeremyVanderMotte <jeremy.vandermotte@gmail.com>
2023-02-10 15:26:34 +0100

    try to fix wordpress

--- srcs/requirements/wordpress/tools/wordpress_install.sh
+++ srcs/requirements/wordpress/tools/wordpress_install.sh
@@ -7,17 +7,15 @@ else
 
 	wget http://wordpress.org/latest.tar.gz
 	tar xfz latest.tar.gz
-	mv wordpress/* .
 	rm -rf latest.tar.gz
-	rm -rf wordpress
 
-	sed -i "s/database_name_here/$DBAME/g" wp-config-sample.php
-	sed -i "s/username_here/$DBUSER/g" wp-config-sample.php
-	sed -i "s/password_here/$DBPWD/g" wp-config-sample.php
-	sed -i "s/localhost/$DBHOST/g" wp-config-sample.php
-
-	sed -i "s/listen =/listen = 0.0.0.0:9000/g" /etc/php/7.3/fpm/pool.d/www.conf
-	mv wp-config-sample.php wp-config.php
+	#sed -i "s/database_name_here/$DBAME/g" wp-config-sample.php
+	#sed -i "s/username_here/$DBUSER/g" wp-config-sample.php
+	#sed -i "s/password_here/$DBPWD/g" wp-config-sample.php
+	#sed -i "s/localhost/$DBHOST/g" wp-config-sample.php
+	sed -i "s/define( 'WP_DEBUG', false );/define( 'WP_DEBUG', true );/g" wordpress/wp-config-sample.php
+	#sed -i "s/listen =/listen = 0.0.0.0:9000/g" /etc/php/7.3/fpm/pool.d/www.conf
+	mv wordpress/wp-config-sample.php wordpress/wp-config.php
 fi
 
 exec "$@"
