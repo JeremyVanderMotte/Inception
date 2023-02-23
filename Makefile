c73a69bed0051c8971fc007936a19d393ed397e8
JeremyVanderMotte <jeremy.vandermotte@gmail.com>
2023-02-23 14:27:21 +0100

    fixed error with wordpress by creating wp-config.php. (always needed by mv wp-config-sample.php to wp-config.php)

--- Makefile
+++ Makefile
@@ -8,8 +8,7 @@ re:
 	@docker compose -f ./srcs/docker-compose.yml up -d --build
 
 clean:
-	@docker stop $$(docker ps -qa); \
-	docker rm $$(docker ps -qa); \
+	@docker rm $$(docker ps -qa); \
 	docker rmi -f $$(docker images -qa); \
 	docker volume rm $$(docker volume ls -q); \
 	docker network rm $$(docker network ls -q);
--- srcs/requirements/wordpress/tools/wordpress_install.sh
+++ srcs/requirements/wordpress/tools/wordpress_install.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+	#Change the listen part for php to listen to wordpress
 	sed -i "s|listen = /run/php/php7.3-fpm.sock|listen = 9000|g" /etc/php/7.3/fpm/pool.d/www.conf
 
+	#Downloading wordpress in path specified
 	wp core download --allow-root --path=/var/www/html/
 
+	sed -i "s/database_name_here/$MARIADB_NAME/g" wp-config-sample.php
+	sed -i "s/username_here/$MARIADB_USER/g" wp-config-sample.php
+	sed -i "s/password_here/$MARIADB_PWD/g" wp-config-sample.php
+	sed -i "s/localhost/$MARIADB_HOST/g" wp-config-sample.php
+	sed -i "s/define( 'WP_DEBUG', false );/define( 'WP_DEBUG', true );/g" wp-config-sample.php
+	mv wp-config-sample.php wp-config.php
+
+	#Command use to skip the installation page 
+	# on wordpress.
 	wp core install \
 		--title=$WP_TITLE \
 		--url=$WP_URL \
@@ -12,32 +23,23 @@
 		--admin_email=$WP_ROOT_EMAIL \
 		--allow-root
 
-	wp config create \
-		--dbname=$MARIADB_NAME\
-		--dbuser=$WP_ROOT_USER \
-		--dbpass=$WP_ROOT_PWD \
-		--dbhost=$MARIADB_HOST \
-		--allow-root
+	#Command used to create the wp-config.php
+	# to skip the config page.
+	#wp config create \
+	#	--dbname=$MARIADB_NAME\
+	#	--dbuser=$WP_ROOT_USER \
+	#	--dbpass=$WP_ROOT_PWD \
+	#	--dbhost=$MARIADB_HOST \
+	#	--allow-root
 	
+	#Command to create a new user in wordpress
 	wp user create \
 	--allow-root \
-	${MARIADB_USER} \
+	${WP_USER} \
 	${WP_USER_EMAIL} \
-	--user_pass=${MARIADB_PWD};
+	--user_pass=${WP_PWD};
 
+	#Install a new theme prettier of the default one.
 	wp theme install hestia --activate --allow-root
-	
-	#wget http://wordpress.org/latest.tar.gz
-	#tar xfz latest.tar.gz
-	#cp -R wordpress/* ./
-	#rm -rf latest.tar.gz
-	#rm -rf wordpress
-	#sed -i "s/database_name_here/$MARIADB_NAME/g" wp-config-sample.php
-	#sed -i "s/username_here/$MARIADB_USER/g" wp-config-sample.php
-	#sed -i "s/password_here/$MARIADB_PWD/g" wp-config-sample.php
-	#sed -i "s/localhost/$MARIADB_HOST/g" wp-config-sample.php
-	#sed -i "s/define( 'WP_DEBUG', false );/define( 'WP_DEBUG', true );/g" wp-config-sample.php
-	#mv wp-config-sample.php wp-config.php
-	
 
 exec "$@"
