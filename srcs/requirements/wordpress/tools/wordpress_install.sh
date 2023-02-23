ab18735abf01206536d6b3749bb7124215cd78a3
JeremyVanderMotte <jeremy.vandermotte@gmail.com>
2023-02-23 13:18:06 +0100

    Working wordpress + creating new user

--- srcs/requirements/wordpress/tools/wordpress_install.sh
+++ srcs/requirements/wordpress/tools/wordpress_install.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
-
 	sed -i "s|listen = /run/php/php7.3-fpm.sock|listen = 9000|g" /etc/php/7.3/fpm/pool.d/www.conf
-	#rm wp-config.php
-	#wp core download --allow-root
 
-	#wp config create --dbname=mariadb \
-	#	--dbuser=jvander \
-	#	--dbpass=1234 \
-	#	--allow-root
+	wp core download --allow-root --path=/var/www/html/
 
-	#wp core install \
-	#	--url="jvander-.42.fr" \
-	#	--admin_user="jvander" \
-	#	--admin_password="1234" \
-	#	--skip-email \
-	#	--allow-root
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	cp -R wordpress/* ./
-	rm -rf latest.tar.gz
-	rm -rf wordpress
+	wp core install \
+		--title=$WP_TITLE \
+		--url=$WP_URL \
+		--admin_user=$MARIADB_ROOT_USER \
+		--admin_password=$MARIADB_ROOT_PWD \
+		--admin_email=$MARIADB_ROOT_EMAIL \
+		--allow-root
 
-	sed -i "s/database_name_here/wordpress/g" wp-config-sample.php
-	sed -i "s/username_here/jvander/g" wp-config-sample.php
-	sed -i "s/password_here/1234/g" wp-config-sample.php
-	sed -i "s/localhost/mariadb/g" wp-config-sample.php
-	sed -i "s/define( 'WP_DEBUG', false );/define( 'WP_DEBUG', true );/g" wp-config-sample.php
-	mv wp-config-sample.php wp-config.php
+	wp config create \
+		--dbname=$MARIADB_NAME\
+		--dbuser=$MARIADB_ROOT_USER \
+		--dbpass=$MARIADB_ROOT_PWD \
+		--allow-root
+	
+	wp user create \
+	--allow-root \
+	${MARIADB_USER} \
+	${MARIADB_USER_EMAIL} \
+	--user_pass=${MARIADB_PWD};
 
+	#wp theme install twentysixsten --activate
+	
+	#wget http://wordpress.org/latest.tar.gz
+	#tar xfz latest.tar.gz
+	#cp -R wordpress/* ./
+	#rm -rf latest.tar.gz
+	#rm -rf wordpress
+	#sed -i "s/database_name_here/$MARIADB_NAME/g" wp-config-sample.php
+	#sed -i "s/username_here/$MARIADB_USER/g" wp-config-sample.php
+	#sed -i "s/password_here/$MARIADB_PWD/g" wp-config-sample.php
+	#sed -i "s/localhost/$MARIADB_HOST/g" wp-config-sample.php
+	#sed -i "s/define( 'WP_DEBUG', false );/define( 'WP_DEBUG', true );/g" wp-config-sample.php
+	#mv wp-config-sample.php wp-config.php
+	
 
 exec "$@"
