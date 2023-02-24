f4f0794ca958db9331861bfbc830ea90bf073da0
JeremyVanderMotte <jeremy.vandermotte@gmail.com>
2023-02-24 15:04:12 +0100

    remove useless command in mariadb and add mkdir folder for data in Makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 all:
+	[ -d /home/jvander/data/wordpress/] || mkdir -p /home/jvander/data/wordpress/
+	[ -d /home/jvander/data/mariadb/] || mkdir -p /home/jvander/data/mariadb/
 	@docker compose -f ./srcs/docker-compose.yml up -d --build
 
 down:
@@ -8,9 +10,16 @@ re:
 	@docker compose -f ./srcs/docker-compose.yml up -d --build
 
 clean:
+	sudo rm -rf /home/jvander/data
 	@docker rm $$(docker ps -qa); \
 	docker rmi -f $$(docker images -qa); \
 	docker volume rm $$(docker volume ls -q); \
 	docker network rm $$(docker network ls -q);
 
-.PHONY: all re down clean
+
+deep: down clean
+	sudo rm -rf ~/data/wordpress/* ;
+	sudo rm -rf ~/data/mariadb/* ;
+	docker system prune
+
+.PHONY: all re down clean deep
--- srcs/requirements/mariadb/tools/install_mariadb.sh
+++ srcs/requirements/mariadb/tools/install_mariadb.sh
@@ -1,42 +1,35 @@
+#Change les droits du dossier et des sous dossier pour y avoir accès
 chown -R mysql:mysql /var/lib/mysql
 
-#Verification que le dossier que va utiliser la basse de donnée à bien été créé dans le bon
-#dossier. 
+#Si la DB existe déjà on ne la recrée aps  
 if [ ! -d /var/lib/mysql/$MARIADB_NAME ]; then
-	#On lance le service mysql mais en lui redéfinissant son directory
+
+	#On lance mysql dans le dossier défini par datadir pour le configurer
 	service mysql start --datadir=/var/lib/mysql
 
-	#On crée un dossier qui va être utiliser par le daemon (le daemon stock toutes les infos
-	#qui vont communiqué ensemble entre les sewrvices dans un même dossier)
+	#Crétion du dossier pour lancer mysqld
 	mkdir -p /var/run/mysqld
 
 	#Une fois les dossiers requis créer on va aussi s'assurer que les documents requis sont
 	#présent. touch va créer un document vide par définition. 
-	touch /var/run/mysqld/mysqlf.pid
+	#touch /var/run/mysqld/mysqlf.pid
 
-	#lance la configuration qui contient les commandes mysql pour créer l'user avec ces permissions
-	#pour wordpress
+	#Lance le script pour créer la DB et un user root
 	eval "echo \"$(cat /tmp/create_database.sql)\"" | mariadb -u root
 
 	#Il faut protéger le root de mysql car on veut que l'user créer précédemment ait accès 
 	#à la database créé mais pas à toutes les databases.
-	mysqladmin -u root password $WP_ROOT_PWD;
+	#mysqladmin -u root password $WP_ROOT_PWD;
 
-	#Je sais pas Pourquoi on arrête le service donc à voir ou à essayer //TO DO
+	
 	service mysql stop --datadir=/var/lib/mysql
 else
 	#On crée un dossier qui va être utiliser par le daemon (le daemon stock toutes les infos
 	#qui vont communiqué ensemble entre les sewrvices dans un même dossier)
-	mkdir -p /var/run/mysqld
-
-	#Une fois les dossiers requis créer on va aussi s'assurer que les documents requis sont
-	#présent. touch va créer un document vide par définition. 
-	touch /var/run/mysqld/mysqlf.pid
-	#Peut être devoir créer le .sock requis dans le document.
-	#touch /var/run/mysqld/mysqld.sock
-
+	#mkdir -p /var/run/mysqld
+	echo "DB $MARIADB_NAME already created..."
 fi
 
-chown -R mysql:mysql /var/run/mysqld
-#Lancement recommandé de MariaDB avec le set-up correct.
+#chown -R mysql:mysql /var/run/mysqld
+#On lance mysql dans le dossier où on l'a installé avant.
 mysqld_safe --datadir=/var/lib/mysql
